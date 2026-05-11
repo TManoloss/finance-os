da0c81b3bd861dbb2bd2695bbcb97907bdbdc8bb
Manoel Carvalho <manoelfelipe90@gmail.com>
2026-05-11 01:50:49 -0300

    feat(mobile): update summary model with new fields

--- mobile/lib/features/dashboard/data/summary_model.dart
+++ mobile/lib/features/dashboard/data/summary_model.dart
@@ -1,19 +1,31 @@
 class FinancialSummary {
   final double totalSpent;
   final double totalReceived;
+  final double checkingBalance;
+  final double creditBalance;
   final List<dynamic> byCategory;
+  final List<dynamic> byDay;
+  final List<dynamic> topMerchants;
 
   FinancialSummary({
     required this.totalSpent,
     required this.totalReceived,
+    required this.checkingBalance,
+    required this.creditBalance,
     required this.byCategory,
+    required this.byDay,
+    required this.topMerchants,
   });
 
   factory FinancialSummary.fromJson(Map<String, dynamic> json) {
     return FinancialSummary(
       totalSpent: (json['total_spent'] as num).toDouble(),
       totalReceived: (json['total_received'] as num).toDouble(),
+      checkingBalance: (json['checking_balance'] as num).toDouble(),
+      creditBalance: (json['credit_balance'] as num).toDouble(),
       byCategory: json['by_category'] ?? [],
+      byDay: json['by_day'] ?? [],
+      topMerchants: json['top_merchants'] ?? [],
     );
   }
 }
