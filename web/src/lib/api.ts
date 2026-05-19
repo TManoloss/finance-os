2bbe1330c8844e34ee183d29e6d4025ee3e4b579
Manoel Carvalho <manoelfelipe90@gmail.com>
2026-05-18 23:13:08 -0300

    fix: add /api/v1 prefix to api base URLs

--- web/src/lib/api.ts
+++ web/src/lib/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getSession, signOut } from "next-auth/react";
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  baseURL: `${process.env.NEXT_PUBLIC_API_URL}/api/v1`,
 });
 
 api.interceptors.request.use(async (config) => {
