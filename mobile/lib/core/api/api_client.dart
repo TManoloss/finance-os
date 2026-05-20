209683b2f74af5c67c54e3fe9fe43aa731244450
Manoel Carvalho <manoelfelipe90@gmail.com>
2026-05-20 00:45:24 -0300

    fix(mobile): configurar baseUrl dinâmica (kReleaseMode) para produção no Render

--- mobile/lib/core/api/api_client.dart
+++ mobile/lib/core/api/api_client.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
@@ -9,7 +10,9 @@ class ApiClient {
     required this.dio,
     required this.storage,
   }) {
-    dio.options.baseUrl = 'http://192.168.1.177:8080/api/v1';
+    dio.options.baseUrl = kReleaseMode
+        ? 'https://finance-os-d3nm.onrender.com/api/v1'
+        : 'http://192.168.1.177:8080/api/v1';
     dio.interceptors.add(
       InterceptorsWrapper(
         onRequest: (options, handler) async {
